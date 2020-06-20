48c19b4dee931896cb56d3fd0471a16f1301a760
Mohamed Jinas <j@live.mv>
2020-06-21 01:25:46 +0500

    Create configs.php

---
+++ configs.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    "shops" => [
+        "Eatmv" => [
+            "name" => "Eat.mv",
+            "description" => "Eat.mv is the Online Supermarket of Maldives.",
+            "domain" => "https://www.eat.mv",
+            "search" => "https://www.eat.mv/catalogsearch/result/?q="
+        ],
+
+        "Dhimart" => [
+            "name" => "dhimart.mv",
+            "description" => "Dhimart, Online Shopping, Supermarket, Marketplace, Maldives, e-commerce, ecommerce",
+            "domain" => "https://dhimart.mv",
+            "search" => "https://dhimart.mv/catalogsearch/result/?q="
+        ]
+    ]
+];
