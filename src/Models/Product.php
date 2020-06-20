8befddb60ba6ca776048d7355690e529d50c1b43
Mohamed Jinas <j@live.mv>
2020-06-21 01:26:14 +0500

    Update Product.php

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -4,14 +4,45 @@ namespace Jinas\Aggregator\Models;
 
 class Product
 {
+    /**
+     * Title of the product
+     *
+     * @var string
+     */
     public $title;
+    /**
+     * Product image stored as a string
+     *
+     * @var string
+     */
     public $image;
+    /**
+     * Product price
+     *
+     * @var int
+     */
     public $price;
 
-    public function __construct(string $title,string $image,string $price) 
+    /**
+     * what Store the product is from
+     *
+     * @var string
+     */
+    public $store;
+
+    /**
+     * __construct
+     *
+     * @param  string $title
+     * @param  string $image
+     * @param  string $price
+     * @return void
+     */
+    public function __construct(string $title, string $image, int $price, string $store)
     {
         $this->title = $title;
         $this->image = $image;
         $this->price = $price;
+        $this->store = $store;
     }
 }
